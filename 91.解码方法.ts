fcce5e10bd8a214806394220ab9490dba62fc6ce
jiangfan233 <lostwind233@outlook.com>
2023-07-22 11:37:02 +0800

    id=91

--- 91.解码方法.ts
+++ 91.解码方法.ts
@@ -7,29 +7,41 @@
 // @lc code=start
 
 function numDecodings(s: string): number {
-  if(s[0] === "0") return 0;
-  let res = [s[0]];
+  if (s[0] === "0") return 0;
+  let dp = Array(s.length).fill(0);
+  dp[0] = 1;
+
+  const check = (i: number) =>
+    s[i - 1] === "1" || (s[i - 1] === "2" && s[i] <= "6");
+
   for (let i = 1; i < s.length; i++) {
-    let tmp : string[] = [];
-    res.forEach((item) => {
-      if(item.length === 1) {
-        let sum = item + s[i];
-        if(sum <= "26") {
-          tmp.push(sum);
-        } 
-        if(s[i] !== "0") {
-          tmp.push(s[i]);
-        }
-      } else if(s[i] !== "0") {
-        tmp.push(s[i]);
+    let tmp = i - 2 < 0 ? 1 : dp[i - 2];
+
+    if (s[i - 1] !== "0" && s[i] !== "0") {
+      if (check(i)) {
+        dp[i] = dp[i - 1] + tmp;
+      } else if (check(i - 1)) {
+        dp[i] = dp[i - 1];
+      } else {
+        dp[i] = tmp;
       }
-    });
-    if(tmp.length === 0) return 0;
-    res = tmp;
-    // console.log(res)
+    }
+
+    if (s[i - 1] !== "0" && s[i] === "0") {
+      if (s[i - 1] >= "3") return 0;
+      dp[i] = tmp;
+    }
+
+    if (s[i - 1] === "0" && s[i] !== "0") {
+      // if (dp[i - 1] === 0) return 0;
+      dp[i] = dp[i - 1];
+    }
+
+    if (s[i - 1] === "0" && s[i] === "0") return 0;
   }
-  return res.length;
+  // console.log(dp);
+  return dp[dp.length - 1];
 }
 // @lc code=end
 
-console.log(numDecodings("111111111111111111111111111111111111111111111"))
+console.log(numDecodings("1201234"));
