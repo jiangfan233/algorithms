c6ec60297ddd56e513d900e55abb33be3145e622
jiang <jiang@localhost.localdomain>
2023-07-24 21:39:43 +0800

    动态规划需要一步步总结规律

--- 96.不同的二叉搜索树.ts
+++ 96.不同的二叉搜索树.ts
@@ -5,19 +5,17 @@
  */
 
 // @lc code=start
-const mul = (i: number): number => i <= 1 ? 1 : i * mul(i - 1);
 
 function numTrees(n: number): number {
-
   let dp = Array(n + 1).fill(0);
   dp[0] = 1;
-
   for(let i = 1; i <= n; i++) {
-    for(let j=1; j<= i; j++) {
+    for(let j = 1; j <= i; j++) {
       dp[i] += dp[j - 1] * dp[i - j];
     }
   }
   return dp[n];
+
 }
 // @lc code=end
 
